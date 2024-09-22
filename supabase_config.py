829046946988ecd81c7b96c0dbcd30a8a3e5b0d4
finn2212 <stolle.finn@googlemail.com>
2024-09-22 16:24:49 +0800

    enew config

--- supabase_config.py
+++ supabase_config.py
@@ -2,6 +2,6 @@ import os
 from supabase import create_client, Client
 
 # Initialize the Supabase client
-SUPABASE_URL = "https://xzelogoeyuszberazhjp.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6ZWxvZ29leXVzemJlcmF6aGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzODE2ODMsImV4cCI6MjA0MTk1NzY4M30.RXPCLpdbQ7w6F_z2ORU8m1jYni5oneSiVkNBpJqodB0"  # Dein Supabase anon key
+SUPABASE_URL = "https://lnacwzshltbjotnpvien.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuYWN3enNobHRiam90bnB2aWVuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxOTExOTM1MCwiZXhwIjoyMDM0Njk1MzUwfQ.ZSmKel80I_o3JbBKbK0plyzxQ-6xOBzx7A4edFjcnLU"  # Dein Supabase anon key
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
